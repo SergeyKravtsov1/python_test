11ea45f7ff3f9be7cac955a1328cbeb0efbb9ffb
SergKravtsov <skravtsov@velitech.com>
2024-10-08 21:49:40 +0300

    first actions

---
+++ Test/Git_test.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pytest
+
+@pytest.fixture()
+def browser():
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
+    return chrome_browser
+        # browser()
--- Test/New.py
+++ Test/New.py
@@ -1,6 +1,21 @@
-def test_one_is_one():
-    assert 1 == 1
-def test_two_is_two():
-    assert 2 == 2
-def test_three_is_three():
-    assert 3 == 2
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+from time import sleep
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://magento.softwaretestingboard.com/women/tops-women/tees-women.html")
+sleep(2)
+girl = driver.find_element(By.XPATH, "//ol[@class='products list items product-items']/li[@class='item product product-item'][1]")
+print(girl.text)
+# print(girl.get_attribute("href"))
+sorter = driver.find_element(By.ID,'sorter')
+select = Select(sorter)
+select.select_by_value('price')
+sleep(2)
+girl = driver.find_element(By.XPATH, "//ol[@class='products list items product-items']/li[@class='item product product-item'][1]")
+print(girl.text)
+# print(girl.get_attribute("href"))
